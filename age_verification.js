9ee0d62649d96e4b357f8e4e300871c65b20fdf1
Taylor A Guy <xXTaylor@gmail.com>
2019-09-14 13:19:36 -0400

    First Try

---
+++ age_verification.js
@@ -0,0 +1,58 @@
+
+var canDrive= function(age) {
+	if(age > 16) {
+	  return true
+	} else {
+	  return false
+	}
+
+}
+
+var canDrink= function(age) {
+	if (age > 21) {
+	  return true
+	} else {
+	  return false
+	}
+
+}
+
+var canRentCar= function(age) {
+	if (age > 25) {
+	  return true
+	} else {
+	  return false
+	}
+
+}
+
+var canPlayBallInPitt= function(age) {
+	if (age > 10) {
+	  return true
+	} else {
+	  return false
+	}
+
+}
+var sarah = 32;
+if (canDrive(sarah)){
+
+//if (canRentCar(sarah)){
+//if (PlayInBallPitt(sarah)){
+
+//console.log("Sarah can drive"; 
+//console.log("Sarah can drink");
+//console.log("Sarah can rent a car");
+//console.log("Sarah can play in the ball pit");
+
+} 
+
+var sarah = 32;
+if (canDrink(sarah)){
+
+console.log("Sarah can drink");
+
+}   
+
+	
+
